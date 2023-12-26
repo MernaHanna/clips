f2fadc2468ce2368cbb85ca95d094bc1afe18582
Merna Hanna <merna.hanna@xlab-group.com>
2023-12-26 19:20:26 +0200

    to 94

---
+++ src/app/shared/tab/tab.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrl: './tab.component.css'
+})
+export class TabComponent implements OnInit {
+
+  @Input() tabTitle: string = '';
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,21 @@
+import { Component, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrl: './tabs-container.component.css'
+})
+export class TabsContainerComponent implements AfterContentInit {
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
+
+  //optional
+  // @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>;
+  
+  constructor() { }
+
+  ngAfterContentInit(): void {
+    console.log(this.tabs);    
+  }
+}
